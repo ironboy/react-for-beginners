6ed34decc0ddb26974a4553b0163f75b833dd0f8
Thomas Frank <thomasfrank@Thomass-MacBook-2.local>
2019-10-22 14:09:36 +0200

    Sort-button working!

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 import GoodThing from './GoodThing';
@@ -6,28 +6,36 @@ import GoodThing from './GoodThing';
 export default () => {
 
   const [goodThings, setGoodThings] = useState([
-    {name: 'Food', prio: 5},
-    {name: 'Coffee', prio: 4},
-    {name: 'Programming', prio: 2},
-    {name: 'My kids', prio: 1},
-    {name: 'Music', prio: 3}
+    { name: 'Food', prio: 5 },
+    { name: 'Coffee', prio: 4 },
+    { name: 'Programming', prio: 2 },
+    { name: 'My kids', prio: 1 },
+    { name: 'Music', prio: 3 }
   ]);
 
+  const [sortOrder, setSortOrder] = useState('unsorted');
+
   const sort = () => {
-    // sort a copy of goodThings and set the new state/value
-    // of goodThings to that of the sorted copy
+    // change the sortOrder and sort according to it
+    setSortOrder(sortOrder !== 'ascending' ? 'ascending' : 'descending');
+    let [x, y] = sortOrder === 'ascending' ? [-1, 1] : [1, -1];
     setGoodThings(
-      [...goodThings].sort((a, b) =>  a.prio < b.prio ? -1 : 1)
+      [...goodThings].sort((a, b) => a.prio < b.prio ? x : y)
     );
   };
 
   return (
     <div className="App">
       <header className="App-header">
-        <button onClick={sort}>Sort</button>
+        <button onClick={sort}>
+          {sortOrder !== 'ascending' ?
+            'Sort most important first' :
+            'Sort least important first'
+          }
+        </button>
         {goodThings.map((thing, index) => (
-          <GoodThing 
-            key={index} 
+          <GoodThing
+            key={index}
             {...thing}
           />
         ))}
