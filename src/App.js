66425ad539768fc87a5fc4b522fc0dcef78f431f
Thomas Frank <thomasfrank@Thomass-MacBook-2.local>
2019-10-22 14:48:50 +0200

    Fetching json data from url

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 import GoodThing from './GoodThing';
 
 export default () => {
 
-  const [goodThings, setGoodThings] = useState([
-    { name: 'Food', prio: 5 },
-    { name: 'Coffee', prio: 4 },
-    { name: 'Programming', prio: 2 },
-    { name: 'My kids', prio: 1 },
-    { name: 'Music', prio: 3 }
-  ]);
-
+  const [goodThings, setGoodThings] = useState([]);
   const [sortOrder, setSortOrder] = useState('unsorted');
+
+  const getData = async () => {
+    // wait until we have the raw data from the resource
+    let rawData = await fetch('/good-things.json');
+    // wait for it to get converted to a javascript 
+    // data structure (unpack from json)
+    let data = await rawData.json();
+    // set good things to the data
+    setGoodThings(data);
+  };
+
+  useEffect(() => {
+    // If no goodThings, then get some good things
+    if(goodThings.length === 0){
+      getData();
+    }
+  });
 
   const sort = () => {
     // change the sortOrder and sort according to it
