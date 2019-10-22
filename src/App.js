b8d9dc0b958b5c41a7bb02d13178dbbe05510ed9
Thomas Frank <thomasfrank@Thomass-MacBook-2.local>
2019-10-22 13:40:42 +0200

    Sort button works

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 import GoodThing from './GoodThing';
 
 export default () => {
 
-  let goodThings = [
+  const [goodThings, setGoodThings] = useState([
     {name: 'Food', prio: 5},
     {name: 'Coffee', prio: 4},
     {name: 'Programming', prio: 2},
     {name: 'My kids', prio: 1},
     {name: 'Music', prio: 3}
-  ];
+  ]);
 
-  goodThings.sort((a, b) =>  a.prio < b.prio ? -1 : 1);
+  const sort = () => {
+    // sort a copy of goodThings and set the new state/value
+    // of goodThings to that of the sorted copy
+    setGoodThings(
+      [...goodThings].sort((a, b) =>  a.prio < b.prio ? -1 : 1)
+    );
+  };
 
   return (
     <div className="App">
       <header className="App-header">
-        <button>Sort</button>
+        <button onClick={sort}>Sort</button>
         {goodThings.map((thing, index) => (
           <GoodThing 
             key={index} 
